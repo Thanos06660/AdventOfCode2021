40fbbcf04762ce76bee43c03598f71dd2338c85b
Thanos06660 <klausschwartz666@gmail.com>
2021-12-16 17:03:21 +0100

    Added Day2

---
+++ AdventOfCode2021/FirstDay/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("First Day - Sonar Sweep - Part One");
+Console.WriteLine("Load Measurements");
+var convertedMeasurements = MeasurementConversion(File.ReadAllLines("SonarMeasurement.txt"));
+Console.WriteLine($"Number of increases {CalculateNumberOfDepthMeasurementIncrease(convertedMeasurements)}");
+Console.WriteLine("First Day - Sonar Sweep - Part Two");
+var slidingWindowsMeasurements = CalculateSlidingWindowsMeasurements(convertedMeasurements);
+Console.WriteLine($"Number of increases {CalculateNumberOfDepthMeasurementIncrease(slidingWindowsMeasurements)}");
+Console.WriteLine("Press <Escape> to quit");
+while (Console.ReadKey().Key != ConsoleKey.Escape) {
+}
+
+static int[] MeasurementConversion(string[] measurements) {
+    var convertedMeasurements = Array.ConvertAll(measurements, s => int.TryParse(s, out var measurement)
+                                                     ? measurement
+                                                     : 0);
+    return convertedMeasurements;
+}
+
+static int CalculateNumberOfDepthMeasurementIncrease(int[] measurements) {
+    var numberOfIncrease = 0;
+    for (var i = 0; i < measurements.Length - 1; i++) {
+        if (Math.Sign(measurements[i + 1] - measurements[i]) > 0) {
+            numberOfIncrease += 1;
+        }
+    }
+
+    return numberOfIncrease;
+}
+
+static int[] CalculateSlidingWindowsMeasurements(int[] measurements) {
+    var slidingWindowsMeasurements = new int[measurements.Length];
+    for (var i = 0; i < measurements.Length - 2; i++) {
+        slidingWindowsMeasurements[i] = measurements.ToList().GetRange(i, 3).Sum();
+    }
+
+    return slidingWindowsMeasurements;
+}
---
+++ AdventOfCode2021/SecondDay/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Day 2 - Dive! - Part One");
+Console.WriteLine("Load SubmarineCourse!");
+var submarineCourse = File.ReadAllLines("SubmarineCourse.txt");
+var position = CalculateHorizontalPositionAndDepth(submarineCourse);
+
+Console.WriteLine($"Number of Horizontalmovement {position.Item1}");
+Console.WriteLine($"Number of Verticalmovement {position.Item2}");
+Console.WriteLine($"Product Of Horizontal and Verticalmovement {position.Item1 * position.Item2}");
+
+Console.WriteLine("Day 2 - Dive! -  Part Two");
+var position2 = CalculateHorizontalPositionAndDepth2(submarineCourse);
+Console.WriteLine($"Number of Horizontalmovement {position2.Item1}");
+Console.WriteLine($"Number of Verticalmovement {position2.Item2}");
+Console.WriteLine($"Product Of Horizontal and Verticalmovement {position2.Item1 * position2.Item2}");
+
+Console.WriteLine("Press <Escape> to quit");
+while (Console.ReadKey().Key != ConsoleKey.Escape) {
+}
+
+static Tuple<int, int> CalculateHorizontalPositionAndDepth(string[] measurements) {
+    var position = new Tuple<int, int>(0, 0);
+    string[] movement;
+    foreach (var m in measurements) {
+        movement = m.Split(' ');
+        int.TryParse(movement[1], out var intResult);
+        switch (movement[0]) {
+            case "forward":
+                position = new Tuple<int, int>(position.Item1 + intResult, position.Item2);
+                break;
+            case "up":
+                position = new Tuple<int, int>(position.Item1, position.Item2 + -1 * intResult);
+                break;
+            case "down":
+                position = new Tuple<int, int>(position.Item1, position.Item2 + intResult);
+                break;
+        }
+    }
+
+    return position;
+}
+
+static Tuple<int, int, int> CalculateHorizontalPositionAndDepth2(string[] measurements) {
+    var position = new Tuple<int, int, int>(0, 0, 0);
+    string[] movement;
+    foreach (var m in measurements) {
+        movement = m.Split(' ');
+        int.TryParse(movement[1], out var intResult);
+        switch (movement[0]) {
+            case "forward":
+                position = new Tuple<int, int, int>(position.Item1 + intResult, position.Item2, position.Item3);
+                position = new Tuple<int, int, int>(position.Item1, position.Item2 + position.Item3 * intResult, position.Item3);
+                break;
+            case "up":
+                position = new Tuple<int, int, int>(position.Item1, position.Item2, position.Item3 + -1 * intResult);
+                break;
+            case "down":
+                position = new Tuple<int, int, int>(position.Item1, position.Item2, position.Item3 + intResult);
+                break;
+        }
+    }
+
+    return position;
+}
+
+
