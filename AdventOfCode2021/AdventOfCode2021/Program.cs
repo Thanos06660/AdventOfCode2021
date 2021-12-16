92129c860436bec8ca9d1806e7baf7f63c8d3baa
Thanos06660 <klausschwartz666@gmail.com>
2021-12-16 13:13:00 +0100

    Added First Day

---
+++ AdventOfCode2021/AdventOfCode2021/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"Number of increases {CalculateNumberOfDepthMeasurementIncrease(File.ReadAllLines("SonarMeasurement.txt"))}");
+Console.ReadKey();
+
+static int CalculateNumberOfDepthMeasurementIncrease(string[] Measurements) {
+    var numberOfIncrease = 0;
+    for (var i = 0; i < Measurements.Length - 1; i++) {
+        int firstMeasurement;
+        int secondMeasurement;
+        int.TryParse(Measurements[i + 1], out secondMeasurement);
+        int.TryParse(Measurements[i], out firstMeasurement);
+        if (Math.Sign(secondMeasurement - firstMeasurement) > 0) {
+            numberOfIncrease += 1;
+        }
+    }
+
+    return numberOfIncrease;
+}
+
+;
